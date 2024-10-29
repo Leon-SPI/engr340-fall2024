817eaef2227ce09f3f78a4b500d8315833d21237
Leon-SPI <paredels@dukes.jmu.edu>
2024-10-29 12:02:05 -0400

    changes

--- Module 7 - Signal Processing/3 - Practice/9.3.1 - Plotting EKG/plot_ekg_template.py
+++ Module 7 - Signal Processing/3 - Practice/9.3.1 - Plotting EKG/plot_ekg_template.py
@@ -1,18 +1,45 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import signal
+
 
 # import the CSV file using numpy
 path = '../../../data/ekg/mitdb_201.csv'
 
 # load data in matrix from CSV file; skip first two rows
+data = np.loadtxt(path, delimiter = ',', skiprows = 2)
 
 ### Your code here ###
+time = data[:, 0]
+lead_signal = data[:, 1]
+V1 = data[:, 2]
+filter_order = 4
+cutoff_frequency = 40
+sample_frequency = 1 / np.mean(np.diff(time))
+print(sample_frequency)
 
-# save each vector as own variable
+nyquist_rate = sample_frequency / 2
+normal_cutoff = cutoff_frequency / nyquist_rate
+butter_lead_signal = signal.butter(filter_order, normal_cutoff, 'low', output = 'sos')
+filtered_lead_signal = signal.sosfilt(butter_lead_signal, lead_signal)
+
+diff_signal = np.diff(filtered_lead_signal, n=1)
+
+sqr_signal = np.square(diff_signal)
+
+window_size = 50
+moving_average_signal = np.convolve(sqr_signal, np.ones(window_size) / window_size, mode = 'same')
 
 ### Your code here ###
+adjusted_time = time[1:]
 
 # use matplot lib to generate a single
-
-### Your code here ###
+plt.figure(figsize=(12, 6))
+plt.plot(adjusted_time, moving_average_signal, color = 'blue')
+plt.title("EKG Lead Signal (MLII vs Time)")
+plt.xlabel("Time (seconds)")
+plt.ylabel("Amplitude (mV)")
+plt.legend()
+plt.grid()
+plt.show()
--- projects/covid-data/student_template.py
+++ projects/covid-data/student_template.py
@@ -209,4 +209,37 @@ if __name__ == "__main__":
     # This is the 7-day period where the number of new cases was maximal.
     third_question(data)
 
+##########################
 
+time = data[:, 0]
+lead_signal = data[:, 1]
+V1 = data[:, 2]
+filter_order = 4
+cutoff_frequency = 40
+sample_frequency = 1 / np.mean(np.diff(time))
+print(sample_frequency)
+
+nyquist_rate = sample_frequency / 2
+normal_cutoff = cutoff_frequency / nyquist_rate
+butter_lead_signal = signal.butter(filter_order, normal_cutoff, 'low', output = 'sos')
+filtered_lead_signal = signal.sosfilt(butter_lead_signal, lead_signal)
+
+diff_signal = np.diff(filtered_lead_signal, n=1)
+
+sqr_signal = np.square(diff_signal)
+
+window_size = 50
+moving_average_signal = np.convolve(sqr_signal, np.ones(window_size) / window_size, mode = 'same')
+
+### Your code here ###
+adjusted_time = time[1:]
+
+# use matplot lib to generate a single
+plt.figure(figsize=(12, 6))
+plt.plot(adjusted_time, moving_average_signal, color = 'blue')
+plt.title("EKG Lead Signal (MLII vs Time)")
+plt.xlabel("Time (seconds)")
+plt.ylabel("Amplitude (mV)")
+plt.legend()
+plt.grid()
+plt.show()
